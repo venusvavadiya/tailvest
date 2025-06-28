de6bdf98747fd815c8301e49131067f03e152429
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2025-06-28 16:12:23 -0500

    frontend cleanup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { App} from './app.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { App } from "./app.tsx";
 
-createRoot(document.getElementById('root')!).render(
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
