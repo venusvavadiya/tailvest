074d3642b51bdc5e25c68d5a04c9131ef7b7117d
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2025-08-29 19:31:57 -0500

    chore setup router and query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,36 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { App } from "./app.tsx";
+import "./custom.css";
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { ThemeProvider } from "next-themes";
+import { StrictMode } from "react";
+import reactDom from "react-dom/client";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({
+  routeTree,
+  pathParamsAllowedCharacters: ["@"],
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rqc = new QueryClient();
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = reactDom.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <ThemeProvider attribute="class">
+        <QueryClientProvider client={rqc}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </StrictMode>,
+  );
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,27 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:8000",
+    },
+  },
 });
